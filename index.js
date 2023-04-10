3f1e6d582b772e58f53e34f147591b0fbc1e192f
EngNelson <bechem200@gmail.com>
2023-04-10 23:41:42 +0100

    newuser created

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const app = express();
 
-app.get("/getMessage", (req, res) => {
-  res.send("Hello Nelson");
+let userList = [
+  {
+    name: "Pedro",
+    age: 19,
+    married: false,
+  },
+  {
+    name: "Pedro",
+    age: 20,
+    married: false,
+  },
+  {
+    name: "Job",
+    age: 20,
+    married: true,
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.json(userList);
+});
+
+app.post("/users", (req, res) => {
+  // Grab data sent by client
+  //   Add data to userList
+  //  Return new list
+
+  const newUser = req.body;
+  userList.push(newUser);
+  res.json(userList);
 });
 
 app.listen("3001", () => {
-  console.log("Server running on port 3001");
+  console.log("Servder running on port 30001");
 });
